53140fdf69c0d2c390d2f7982c96b759afcdf1a4
Ewan Cahen <e.cahen@esciencecenter.nl>
2022-08-04 12:09:16 +0200

    add documentation and a script that allows you to upgrade to release version 1.2.2

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # RSD-production
 
 This repository contains the necessary scripts and information needed to run an RSD in production
+
+## Database migration
+When upgrading to a new release, the database structure might change. See the `database-migration` directory for instructions.
+
+Starting from release 1.2.2, we also provide the necessary scripts that allow you to update the database between a release and its immediate predecessor (see the `migration-scripts` directory).
--- database-migration/migration-scripts/1.2.1-to-1.2.2.sql
+++ database-migration/migration-scripts/1.2.1-to-1.2.2.sql
@@ -1,3 +1,6 @@
+---------- CREATED MANUALLY ----------
+
+-- It is advised to first turn off the scrapers before running this script as they might insert new duplicates
 -- Check for duplicate DOI's in the mention, software and release_content tables respectively
 SELECT LOWER(doi), COUNT(doi) FROM mention GROUP BY LOWER(doi) HAVING COUNT(doi) > 1;
 
@@ -6,6 +9,7 @@ SELECT LOWER(concept_doi), COUNT(concept_doi) FROM software GROUP BY LOWER(conce
 SELECT LOWER(doi), COUNT(doi) FROM release_content GROUP BY LOWER(doi) HAVING COUNT(doi) > 1;
 
 -- We only have duplicates in the mention table. Therefore, we only need to deduplicate those.
+-- IF YOU HAVE ANY DUPLICATES IN ONE OF THE TWO OTHER TABLES, YOU NEED TO WRITE CODE THAT DEDUPLICATES THIS YOURSELF!
 -- We can not simply delete duplicates, as they might be referenced to in the mention_for_software, impact_for_project or output_for_project table.
 -- First (arbitrarily) select mentions to keep:
 SELECT DISTINCT ON (LOWER(doi)) * INTO TEMP mentions_to_keep FROM mention WHERE LOWER(doi) IN (SELECT LOWER(doi) FROM mention GROUP BY LOWER(doi) HAVING COUNT(doi) > 1);
@@ -22,3 +26,39 @@ UPDATE output_for_project SET mention = (SELECT id_keep FROM map_delete_to_keep 
 
 -- Finally, we can delete the duplicate mentions
 DELETE FROM mention WHERE id IN (SELECT id FROM mentions_to_delete);
+
+
+---------- CREATED BY MIGRA ----------
+
+alter table "public"."mention" drop constraint "mention_doi_check";
+
+alter table "public"."software" drop constraint "software_concept_doi_check";
+
+drop function if exists "public"."software_join_release"();
+
+alter table "public"."mention" alter column "doi" set data type citext using "doi"::citext;
+
+alter table "public"."release_content" alter column "doi" set data type citext using "doi"::citext;
+
+alter table "public"."software" alter column "concept_doi" set data type citext using "concept_doi"::citext;
+
+alter table "public"."release_content" add constraint "release_content_doi_check" CHECK (((doi ~ '^10(\.\w+)+/\S+$'::citext) AND (length((doi)::text) <= 255)));
+
+alter table "public"."mention" add constraint "mention_doi_check" CHECK (((doi ~ '^10(\.\w+)+/\S+$'::citext) AND (length((doi)::text) <= 255)));
+
+alter table "public"."software" add constraint "software_concept_doi_check" CHECK (((concept_doi ~ '^10(\.\w+)+/\S+$'::citext) AND (length((concept_doi)::text) <= 255)));
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.software_join_release()
+ RETURNS TABLE(software_id uuid, slug character varying, concept_doi citext, release_id uuid, releases_scraped_at timestamp with time zone)
+ LANGUAGE plpgsql
+ STABLE
+AS $function$
+BEGIN
+	RETURN QUERY SELECT software.id AS software_id, software.slug, software.concept_doi, release.id AS release_id, release.releases_scraped_at FROM software LEFT JOIN RELEASE ON software.id = RELEASE.software;
+	RETURN;
+END
+$function$
+;
+
