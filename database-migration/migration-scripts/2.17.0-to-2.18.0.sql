20ecf366fc90ab98f971d94e700dd464d1606dd3
ewan-escience <91328544+ewan-escience@users.noreply.github.com>
2024-07-12 11:42:16 +0200

    Update 2.17.0-to-2.18.0.sql

--- database-migration/migration-scripts/2.17.0-to-2.18.0.sql
+++ database-migration/migration-scripts/2.17.0-to-2.18.0.sql
@@ -2,6 +2,20 @@
 
 alter table "public"."category" drop constraint "invalid_value_for_properties";
 
+-- added manually
+
+ALTER TABLE image DROP CONSTRAINT IF EXISTS image_valid_mime_type;
+
+-- You might have to run the following if you have image/jpg in your database:
+
+-- UPDATE IMAGE SET mime_type = 'image/jpeg' WHERE mime_type = 'image/jpg';
+
+-- if you have more conflicts, run the following and adapt the constraint:
+
+-- select mime_type, count(*) from image group by mime_type order by mime_type;
+
+-- end added manually
+
 alter table "public"."image" add constraint "image_valid_mime_type" CHECK (((mime_type)::text = ANY ((ARRAY['image/avif'::character varying, 'image/gif'::character varying, 'image/jpeg'::character varying, 'image/png'::character varying, 'image/svg+xml'::character varying, 'image/webp'::character varying, 'image/x-icon'::character varying])::text[])));
 
 alter table "public"."category" add constraint "invalid_value_for_properties" CHECK (((properties - '{icon,is_highlight,description,subtitle,tree_level_labels}'::text[]) = '{}'::jsonb));
